30758e3a2e2017c8eedba64b450c3b2758e5fbc1
Anthony Lau <anthonycylau@gmail.com>
2019-01-26 00:30:27 -0600

    feat: 🎸 Adds header to project
    BREAKING CHANGE: N
    
    Issues: N

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { PageHeader, Grid, Row, Col } from 'react-bootstrap'
+import './Header.css'
+
+class Header extends Component {
+  
+  render() {
+    return (
+      <PageHeader className="headerStyle">
+        <Grid>
+          <Row>
+            <Col xsOffset={6}>
+              Anthony Lau
+            </Col>
+          </Row>
+        </Grid>
+      </PageHeader>
+    );
+  }
+}
+
+export default Header;
