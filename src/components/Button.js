5a846e1cdd519150cd976eca376a0a6ab383a889
Anthony Lau <anthonycylau@gmail.com>
2019-01-26 02:17:34 -0600

    style: 💄 Changed color of buttons
    BREAKING CHANGE: N
    
    Issues: N

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ class Button extends Component {
   render() {
     return (
       <div className="button">
-        <a href={this.props.link} class="btn btn-default">{this.props.socialMedia}</a>
+        <a href={this.props.link} class="btn btn-lg" style={{color: "#2c2b2c"}}>{this.props.socialMedia}</a>
       </div>
     );
   }
