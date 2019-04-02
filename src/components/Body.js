4384353637505542325cbf726bf7a927f8431f42
Anthony Lau <anthonycylau@gmail.com>
2019-04-01 23:13:03 -0500

    added initial phone border

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import Button from './Button.js'
+import Phone from './Phone.js'
 import './Body.css'
 
 class Body extends Component {
   render() {
     return (
-      <div className="footer">
-        <Button text="LinkedIn" link="https://www.linkedin.com/in/anthony-lau-29a738117"></Button>
-        <Button text="Github" link="https://github.com/Anthony-Lau"></Button>
+      <div className="body">
+        <Phone/>
+        <div className="footer">
+          <Button text="LinkedIn" link="https://www.linkedin.com/in/anthony-lau-29a738117"></Button>
+          <Button text="Github" link="https://github.com/Anthony-Lau"></Button>
+        </div>
       </div>
     );
   }
---
+++ src/components/Phone.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './Phone.css'
+
+class Phone extends Component {
+  render() {
+    return (
+      <div className="center">
+        <div className="phone-border">
+          <div className="phone-shade">
+            <div className="phone">
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Phone;
